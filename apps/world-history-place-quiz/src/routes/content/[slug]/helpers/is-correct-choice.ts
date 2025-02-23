67079f8b51211edc338b9115014b2e6657c1f9e8
Akihiro Tojo <akihiro.021109@gmail.com>
2025-02-23 14:18:37 +0900

    feat(content): create content page

---
+++ apps/world-history-place-quiz/src/routes/content/[slug]/helpers/is-correct-choice.ts
@@ -0,0 +1,14 @@
+import type { ChoiceProps } from "../../../../components/choice/choice.svelte";
+
+export const isCorrectChoice = (
+	choiceIndex: number,
+	selectedChoice: number,
+	correctChoice: number,
+): ChoiceProps["isCorrect"] => {
+	const isSelected = choiceIndex === selectedChoice;
+	const isCorrect = choiceIndex === correctChoice;
+	if (isSelected || isCorrect) {
+		return isCorrect;
+	}
+	return null;
+};
