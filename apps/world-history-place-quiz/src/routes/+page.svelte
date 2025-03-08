c4d116bccd94bd65459c806f909f23d4271b3261
Akihiro Tojo <akihiro.tojo.02+github@gmail.com>
2025-03-08 14:21:38 +0900

    feat(whpq): add meta tags (#17)
    * feat(seo): replace lang attribute `en` with `ja`
    
    * feat(seo): add common meta tags
    
    * feat(seo): add meta tags to top page
    
    * feat(seo): add meta tags to content page

--- apps/world-history-place-quiz/src/routes/+page.svelte
+++ apps/world-history-place-quiz/src/routes/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
+import { SITE_ORIGIN, SITE_NAME } from "./constant";
+
 const { data } = $props();
 </script>
+
+<svelte:head>
+  <title>{SITE_NAME}</title>
+  <meta property="og:title" content={SITE_NAME} />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content={SITE_ORIGIN} />
+  <meta property="og:image" content={`${SITE_ORIGIN}/og-image.png`} />
+</svelte:head>
 
 <ul>
   {#each data.contents as content (content.slug)}
     <li>
-      <a href={`/content/${content.slug}`}>
+      <a href={content.path}>
         {content.title}
       </a>
     </li>
