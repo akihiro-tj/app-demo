5529a18821f40803fffacef1699b63d542caf367
Akihiro Tojo <akihiro.tojo.02+github@gmail.com>
2025-03-30 18:07:56 +0900

    feat(whpq): sort contents into the configured order (#53)
    * feat(entity): implement content order entity
    
    * feat(repository): implement content order repository
    
    * feat(usecase): implement content order usecase

--- apps/world-history-place-quiz/src/usecases/content.ts
+++ apps/world-history-place-quiz/src/usecases/content.ts
@@ -1,9 +1,12 @@
 import type { Content } from "@/entities/content";
 import type { Question } from "@/entities/question";
 import { ContentRepository } from "@/repositories/content";
+import { ContentsOrderRepository } from "@/repositories/contents-order";
 import type { Content as ContentForService } from "@/routes/types/content";
 import type { Question as QuestionForService } from "@/routes/types/question";
 import { FileLoader } from "@app-demo/file-loader";
+
+const CONTENTS_PATH = "./contents/";
 
 type Entry = {
 	slug: string;
@@ -13,9 +16,18 @@ const getContentRepository = (): ContentRepository => {
 	const fileLoader = new FileLoader();
 	const contentRepository = new ContentRepository({
 		fileLoader,
-		dataPath: "./contents/",
+		dataPath: CONTENTS_PATH,
 	});
 	return contentRepository;
+};
+
+const getContentsOrderRepository = (): ContentsOrderRepository => {
+	const fileLoader = new FileLoader();
+	const contentsOrderRepository = new ContentsOrderRepository({
+		fileLoader,
+		dataPath: CONTENTS_PATH,
+	});
+	return contentsOrderRepository;
 };
 
 const mapContent = (content: Content): ContentForService => {
@@ -37,6 +49,20 @@ const mapContent = (content: Content): ContentForService => {
 	};
 };
 
+const sortContents = (contents: ContentForService[]): ContentForService[] => {
+	const contentsOrderRepository = getContentsOrderRepository();
+	const contentsOrder = contentsOrderRepository.get();
+	const sortedContents = contents.sort((a, b) => {
+		const aIndex = contentsOrder.contentIds.indexOf(a.id);
+		const bIndex = contentsOrder.contentIds.indexOf(b.id);
+		if (aIndex === -1 && bIndex === -1) return 0;
+		if (aIndex === -1) return 1;
+		if (bIndex === -1) return -1;
+		return aIndex - bIndex;
+	});
+	return sortedContents;
+};
+
 export const getContent = (contentId: string): ContentForService => {
 	const contentRepository = getContentRepository();
 	const content = contentRepository.get({ contentId });
@@ -48,7 +74,8 @@ export const getAllContents = (): ContentForService[] => {
 	const contentRepository = getContentRepository();
 	const contents = contentRepository.getAll();
 	const mappedContents = contents.map(mapContent);
-	return mappedContents;
+	const sortedContents = sortContents(mappedContents);
+	return sortedContents;
 };
 
 export const getContentsEntries = (): Entry[] => {
