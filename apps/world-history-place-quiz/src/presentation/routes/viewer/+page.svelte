cd6ace5b243a0dac018e9c953840ed2da7b26123
Akihiro Tojo <akihiro.021109@gmail.com>
2025-05-08 23:14:38 +0900

    feat(view): add viewer page

---
+++ apps/world-history-place-quiz/src/presentation/routes/viewer/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { Deck, type MapViewState } from "@deck.gl/core";
+import { onMount } from "svelte";
+import { getLandTileLayer } from "./layers/land-tile";
+import { getRegionTileLayer } from "./layers/region-tile";
+import { canvasStyle, mainColumnStyle } from "./styles";
+
+let deckCanvas: HTMLCanvasElement;
+
+const INITIAL_VIEW_STATE: MapViewState = {
+	longitude: 18.382992,
+	latitude: 45.404543,
+	zoom: 3,
+	minZoom: 0,
+	maxZoom: 10,
+};
+
+const { data } = $props();
+const { geoFeatures } = data;
+
+const render = () => {
+	new Deck({
+		canvas: deckCanvas,
+		initialViewState: INITIAL_VIEW_STATE,
+		controller: true,
+		layers: [getLandTileLayer(), getRegionTileLayer(geoFeatures)],
+	});
+};
+
+onMount(() => {
+	render();
+});
+</script>
+
+<svelte:head></svelte:head>
+
+<main class={mainColumnStyle}>
+	<canvas bind:this={deckCanvas} class={canvasStyle}></canvas>
+</main>
