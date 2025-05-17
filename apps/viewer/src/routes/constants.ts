41bd5ba9f6b86cf0d5510d75a2aeea5a6553e53d
Akihiro Tojo <akihiro.021109@gmail.com>
2025-05-17 23:22:16 +0900

    feat(viewer): add viewer page

---
+++ apps/viewer/src/routes/constants.ts
@@ -0,0 +1,29 @@
+import type { MapViewState } from "@deck.gl/core";
+
+// TODO: Replace this with the actual site name
+export const SITE_NAME = "世界史地図";
+export const SITE_ORIGIN = "https://world-history-place-quiz.vercel.app";
+
+export enum GeoFeatureCategory {
+	MOUNTAIN = "mountain",
+	ISLAND = "island",
+}
+
+export const GEO_FEATURE_CATEGORY_NAMES: Record<GeoFeatureCategory, string> = {
+	[GeoFeatureCategory.MOUNTAIN]: "山脈",
+	[GeoFeatureCategory.ISLAND]: "島",
+};
+
+export enum CursorState {
+	GRAB = "grab",
+	GRABBING = "grabbing",
+	POINTER = "pointer",
+}
+
+export const INITIAL_VIEW_STATE: MapViewState = {
+	longitude: 18.382992,
+	latitude: 45.404543,
+	zoom: 3,
+	minZoom: 0,
+	maxZoom: 10,
+};
