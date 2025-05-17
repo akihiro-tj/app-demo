58479d0b6877a7691f792ff36a82e83c1e2d4a07
Akihiro Tojo <akihiro.tojo.02+github@gmail.com>
2025-05-17 23:29:28 +0900

    feat(viewer): add viewer app (#85)
    * feat(viewer): setup project (#83)
    
    * chore(map): initialize project
    
    * chore(map): add biome settings
    
    * chore(map): exec `pnpm check:write`
    
    * chore(map): add ts settings
    
    * chore(map): move svelte to dependencies
    
    * chore(map): add svelte-kit
    
    * chore(map): add panda css
    
    * chore(map): remove unnecessary file
    
    * chore(map): rename project
    
    * feat(viewer): add viewer page (#84)
    
    * chore(deps): add deck.gl
    
    * chore(deps): add vitest
    
    * feat(viewer): add viewer page
    
    * chore(deps): add vitest
    
    * feat(viewer): add viewer page

---
+++ apps/viewer/src/components/header/header.svelte
@@ -0,0 +1,11 @@
+<script>
+import { SITE_NAME } from "@/routes/constants";
+import { headerStyle, titleContainerStyle, titleStyle } from "./header.styles";
+</script>
+
+<!-- TODO: Move this component to packages -->
+<header class={headerStyle}>
+  <div class={titleContainerStyle}>
+    <a class={titleStyle} href="/">{SITE_NAME}</a>
+  </div>
+</header>
