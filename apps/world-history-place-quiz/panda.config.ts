6de48ae51287d6627266ce7975169d9da8f9a52c
Akihiro Tojo <akihiro.021109@gmail.com>
2025-05-18 11:40:27 +0900

    chore(apps): use common panda config

--- apps/world-history-place-quiz/panda.config.ts
+++ apps/world-history-place-quiz/panda.config.ts
@@ -1,31 +1,3 @@
-import { defineConfig, defineGlobalStyles } from "@pandacss/dev";
+import { pandaConfig } from "@app-demo/panda-config";
 
-const globalCss = defineGlobalStyles({
-	html: {
-		"--global-font-body": "Inter Variable, Noto Sans JP Variable, sans-serif",
-		// TODO: Add monospace fonts
-		// "--global-font-mono": "",
-		color: "slate.900",
-	},
-});
-
-export default defineConfig({
-	// Whether to use css reset
-	preflight: true,
-
-	// Where to look for your css declarations
-	include: ["./src/**/*.{js,ts,svelte}"],
-
-	// Files to exclude
-	exclude: [],
-
-	// Useful for theme customization
-	theme: {
-		extend: {},
-	},
-
-	// The output directory for your css system
-	outdir: "styled-system",
-
-	globalCss,
-});
+export default pandaConfig;
