886bfa43a340f000ad223e47f83023203b3efeac
Akihiro Tojo <akihiro.tojo.02+github@gmail.com>
2025-05-18 15:04:56 +0900

    chore(app): add boilerplate (#92)

---
+++ apps/boilerplate/src/index.test.ts
@@ -0,0 +1,5 @@
+import { expect, it } from "vitest";
+
+it("true is true", () => {
+	expect(true).toBe(true);
+});
---
+++ apps/boilerplate/svelte.config.js
@@ -0,0 +1,8 @@
+import { baseConfig } from "@app-demo/svelte-config";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	...baseConfig,
+};
+
+export default config;
