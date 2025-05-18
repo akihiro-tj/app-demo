da00b9ce922ebe3c77c084baded844e561f2d94c
Akihiro Tojo <akihiro.tojo.02+github@gmail.com>
2025-05-18 15:04:56 +0900

    chore(app): add boilerplate (#92)

---
+++ apps/boilerplate/vite.config.ts
@@ -0,0 +1,7 @@
+import { baseConfig } from "@app-demo/vite-config";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	plugins: [sveltekit(), baseConfig()],
+});
