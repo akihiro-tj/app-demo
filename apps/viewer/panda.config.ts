e049c43c74e26481ffb7fd2536f0c43e7bef5960
Akihiro Tojo <akihiro.021109@gmail.com>
2025-05-18 13:39:56 +0900

    fix

--- apps/viewer/panda.config.ts
+++ apps/viewer/panda.config.ts
@@ -1,4 +1,31 @@
-import { baseConfig } from "@app-demo/panda-config";
-import { defineConfig } from "@pandacss/dev";
+import { type Config, defineConfig, defineGlobalStyles } from "@pandacss/dev";
+
+const globalCss = defineGlobalStyles({
+	html: {
+		"--global-font-body": "Inter Variable, Noto Sans JP Variable, sans-serif",
+		color: "slate.900",
+	},
+});
+
+export const baseConfig: Config = {
+	// Whether to use css reset
+	preflight: true,
+
+	// Where to look for your css declarations
+	include: ["./src/**/*.{js,ts,svelte}"],
+
+	// Files to exclude
+	exclude: [],
+
+	// Useful for theme customization
+	theme: {
+		extend: {},
+	},
+
+	// The output directory for your css system
+	outdir: "styled-system",
+
+	globalCss,
+};
 
 export default defineConfig(baseConfig);
