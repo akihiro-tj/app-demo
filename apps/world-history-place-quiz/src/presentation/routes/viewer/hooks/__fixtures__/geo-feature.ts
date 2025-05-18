233637241b62d0d893b01e9ec678a6e0097f7c4d
Akihiro Tojo <akihiro.021109@gmail.com>
2025-05-18 15:40:39 +0900

    chore(quiz): remove unnecessary viewer files

--- apps/world-history-place-quiz/src/presentation/routes/viewer/hooks/__fixtures__/geo-feature.ts
+++
@@ -1,9 +0,0 @@
-import { GeoFeatureCategory } from "@/domain/models/geo-feature";
-import type { GeoFeatureViewModel } from "@/presentation/models/geo-feature";
-export const mockGeoFeatures: GeoFeatureViewModel[] = [
-	{
-		id: 1159103941,
-		name: "ピレネー山脈",
-		category: GeoFeatureCategory.MOUNTAIN,
-	},
-];
