3231783a6265defc9627318609201baddee3bb84
Akihiro Tojo <akihiro.021109@gmail.com>
2025-05-22 21:28:16 +0900

    feat: add header to ui

---
+++ packages/ui/src/lib/header/header.styles.ts
@@ -0,0 +1,18 @@
+import { css } from "@world-history-map/styled-system/css";
+
+export const headerStyle = css({
+	p: "4",
+	borderBottomWidth: "1px",
+	borderBottomStyle: "solid",
+	borderBottomColor: "slate.200",
+});
+
+export const titleContainerStyle = css({
+	maxW: "4xl",
+	mx: "auto",
+});
+
+export const titleStyle = css({
+	fontSize: "xl",
+	fontWeight: "bold",
+});
