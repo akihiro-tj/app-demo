0025731c6f870bf44a24ebd23d5002183bc52e42
Akihiro Tojo <akihiro.tojo.02+github@gmail.com>
2025-06-03 22:47:44 +0900

    build: fix output path of top (#136)

--- scripts/aggregate.mjs
+++ scripts/aggregate.mjs
@@ -1,10 +1,13 @@
 import fs from "node:fs";
 
-const APPS = ["quiz", "top", "viewer"];
+const APPS = ["quiz", "viewer"];
 const OUTPUT_DIR_PATH = "./build";
 
 fs.rmSync(OUTPUT_DIR_PATH, { recursive: true, force: true });
 fs.mkdirSync(OUTPUT_DIR_PATH, { recursive: true });
+
+const TOP_DIR_PATH = "apps/top/build";
+fs.cpSync(TOP_DIR_PATH, OUTPUT_DIR_PATH, { recursive: true });
 
 for (const app of APPS) {
 	const appDirPath = `apps/${app}/build`;
