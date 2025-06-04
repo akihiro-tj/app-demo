29f81eed11471988ced6ceec3fbf146a7040c5f6
Akihiro Tojo <akihiro.021109@gmail.com>
2025-06-04 22:26:36 +0900

    chore(viewer): replace pmtiles source

--- apps/viewer/src/routes/hooks/layers/constants.ts
+++ apps/viewer/src/routes/hooks/layers/constants.ts
@@ -1,8 +1,11 @@
-import { base } from "$app/paths";
 import type { Color } from "@deck.gl/core";
 import { hexToRgb } from "../../utils/hex-to-rgb";
 
-export const LAND_TILE_SOURCE_URL = `${base}/tiles/land.pmtiles`;
+const ASSET_BASE_URL = import.meta.env.DEV
+	? "https://d1dxhjxis2et39.cloudfront.net"
+	: "https://d1qn6pwkdv9f28.cloudfront.net";
+
+export const LAND_TILE_SOURCE_URL = `${ASSET_BASE_URL}/tiles/land.pmtiles`;
 export const LAND_TILE_LAYER_ID = "land-tile-layer";
 
 export const COLOR_TRANSPARENT: Color = [0, 0, 0, 0];
@@ -11,11 +14,11 @@ export const COLOR_BACKGROUND: Color = hexToRgb("#94a3b8");
 
 export const FILL_OPACITY = 0.8;
 
-export const MOUNTAIN_TILE_SOURCE_URL = `${base}/tiles/mountain.pmtiles`;
-export const PLATEAU_TILE_SOURCE_URL = `${base}/tiles/plateau.pmtiles`;
-export const DESERT_TILE_SOURCE_URL = `${base}/tiles/desert.pmtiles`;
-export const ISLAND_TILE_SOURCE_URL = `${base}/tiles/island.pmtiles`;
-export const PENINSULA_TILE_SOURCE_URL = `${base}/tiles/peninsula.pmtiles`;
+export const MOUNTAIN_TILE_SOURCE_URL = `${ASSET_BASE_URL}/tiles/mountain.pmtiles`;
+export const PLATEAU_TILE_SOURCE_URL = `${ASSET_BASE_URL}/tiles/plateau.pmtiles`;
+export const DESERT_TILE_SOURCE_URL = `${ASSET_BASE_URL}/tiles/desert.pmtiles`;
+export const ISLAND_TILE_SOURCE_URL = `${ASSET_BASE_URL}/tiles/island.pmtiles`;
+export const PENINSULA_TILE_SOURCE_URL = `${ASSET_BASE_URL}/tiles/peninsula.pmtiles`;
 
 export const MOUNTAIN_TILE_LAYER_ID = "mountain-tile-layer";
 export const PLATEAU_TILE_LAYER_ID = "plateau-tile-layer";
