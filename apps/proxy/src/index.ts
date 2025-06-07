540e41509fe89fae558a6fde62c2290ea6ed33c9
Akihiro Tojo <akihiro.021109@gmail.com>
2025-06-07 20:44:03 +0900

    feat(proxy): initialize proxy app

---
+++ apps/proxy/src/index.ts
@@ -0,0 +1,29 @@
+import { Hono, type Context } from "hono";
+
+type Bindings = {
+	TOP_APP_URL: string;
+	QUIZ_APP_URL: string;
+	VIEWER_APP_URL: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get("/quiz/*", async (c: Context) => {
+	const path = c.req.path;
+	const response = await fetch(`${c.env.QUIZ_APP_URL}${path}`);
+	return response;
+});
+
+app.get("/top/*", async (c: Context) => {
+	const path = c.req.path;
+	const response = await fetch(`${c.env.TOP_APP_URL}${path}`);
+	return response;
+});
+
+app.get("/viewer/*", async (c: Context) => {
+	const path = c.req.path;
+	const response = await fetch(`${c.env.VIEWER_APP_URL}${path}`);
+	return response;
+});
+
+export default app;
